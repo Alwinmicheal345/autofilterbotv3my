fba6c80092391cab16125455af99eef94d012e73
Alwinmicheal345 <93310248+Alwinmicheal345@users.noreply.github.com>
2021-10-30 12:39:07 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -30,12 +30,12 @@ Bot Owner Only
 ABOUT = """
 ➥ Name :  Auto Filter V3
     
-➥ Creator : [Naughty PROFESSOR](
-https://youtube.com/c/NaughtyPROFESSOR)
+➥ Creator : [Alwin Micheal](
+https://t.me/new_movie_HD_movies)
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Tutorial : [Click Me](https://youtu.be/5hnYOKBzyi8)
+➥ Tutorial : [Click Me](https://t.me/new_movie_HD_movies)
 """
